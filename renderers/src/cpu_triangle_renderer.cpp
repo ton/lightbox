5e09d94a4fefb6477bd0673a9f7f48452ba96512
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2013-12-20 17:55:49 +0100

    Render an animating quad instead of a triangle.
    This illustrates an issue with the intersection method where the edge of
    a triangle is not properly rendered.

--- renderers/src/cpu_triangle_renderer.cpp
+++ renderers/src/cpu_triangle_renderer.cpp
@@ -17,7 +17,8 @@ CpuTriangleRenderer::CpuTriangleRenderer():
 void CpuTriangleRenderer::render(unsigned int width, unsigned int height, uint32_t *pixels) const
 {
     static int j = 0;
-    Triangle t(Vertex(0.0 + j % 200, 0.0, 0.0), Vertex(200.0 + j % 200, 0.0, 0.0), Vertex(100.0 + j % 200, 200.0, 0.0));
+    Triangle a(Vertex(0.0 + j % 200, 0.0, 0.0), Vertex(200.0 + j % 200, 0.0, 0.0), Vertex(200.0 + j % 200, 200.0, 0.0));
+    Triangle b(Vertex(0.0 + j % 200, 0.0, 0.0), Vertex(200.0 + j % 200, 200.0, 0.0), Vertex(0.0 + j % 200, 200.0, 0.0));
     j += 2;
 
     uint32_t *pixel = &(pixels[0]);
@@ -29,7 +30,7 @@ void CpuTriangleRenderer::render(unsigned int width, unsigned int height, uint32
         {
             ray.d.x += 1;
 
-            if (ray.intersectsMollerTrumbore(t))
+            if (ray.intersectsMollerTrumbore(a) || ray.intersectsMollerTrumbore(b))
             {
                 *pixel++ = (0xff << 16) + (0xff << 8) + 0xff;
             }
